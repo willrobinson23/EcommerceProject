6387f261d581e5f62808f02b34fe5a387e6d0484
Ankit Poudel <crankit694@gmail.com>
2023-08-21 11:37:08 +0545

    initial commit

--- script.js
+++ script.js
@@ -10,19 +10,22 @@ const A = document.getElementById('cross');
 
 const image = document.getElementById('main');
 const image2 = document.getElementsByClassName('smallimg1');
-
-
+const price = document.querySelector('.op');
 
     image2[0].onclick = function(){
         image.src = image2[0].src;
+        price.innerText = '$78.56';
     }
     image2[1].onclick = function(){
         image.src = image2[1].src;
+        price.innerText = '$56.99';
     }
     image2[2].onclick = function(){
         image.src = image2[2].src;
+        price.innerText = '$63.78';
     }
     image2[3].onclick = function(){
         image.src = image2[3].src;
+        price.innerText = '$60.43';
     }
     
